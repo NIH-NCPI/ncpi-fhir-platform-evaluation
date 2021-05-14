3f3dd3c7bcfe4735b9b48a95e5fba2d2a91f9cad
Eric Torstenson <eric.s.torstenson@vumc.org>
2021-05-14 16:36:32 -0500

    Dropped extension from script

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
-    scripts=['scripts/evaluate_server.py'],
+    scripts=['scripts/evaluate_server'],
 )
