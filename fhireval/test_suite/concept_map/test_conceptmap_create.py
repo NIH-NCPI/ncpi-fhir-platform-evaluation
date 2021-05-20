11f731857c19ac245b7501cd70d6cfee18a3b2d4
Eric Torstenson <eric.s.torstenson@vumc.org>
2021-05-20 16:53:56 -0500

    Migrate test concept map over to new data system

--- fhireval/test_suite/concept_map/test_conceptmap_create.py
+++ fhireval/test_suite/concept_map/test_conceptmap_create.py
@@ -1,14 +1,16 @@
 import pytest
 import pdb
 
-from fhireval.test_suite.concept_map import example_code_system_source, reset_testdata
+from fhireval.test_suite.concept_map import reset_testdata
+from fhireval.test_suite.crud import prep_server
 
 test_id = f"{'2.8.1':<10} - Create ConceptMap"
 
 test_weight = 2
 
-def test_codemap_create(host):
-    reset_testdata(host)
+def test_codemap_create(host, prep_server):
+    reset_testdata(host, prep_server)
+    example_code_system_source = prep_server['CMG']['ConceptMap'][0]
     result = host.post('ConceptMap', example_code_system_source, validate_only=False)
     assert result['status_code'] == 201
     cm_id = result['response']['id']
--- fhireval/test_suite/concept_map/test_conceptmap_translate.py
+++ fhireval/test_suite/concept_map/test_conceptmap_translate.py
@@ -1,20 +1,22 @@
 import pytest
 import pdb
-from fhireval.test_suite.concept_map import example_code_system_source, reset_testdata
+from fhireval.test_suite.concept_map import reset_testdata
 from fhirwood.parameters import ConceptMapParameter
+from fhireval.test_suite.crud import prep_server
 
 test_id = f"{'2.8.2':<10} - ConceptMap Translate"
 
 test_weight = 2
 
-def test_codemap_translate(host):
-    reset_testdata(host)
+def test_codemap_create(host, prep_server):
+    reset_testdata(host, prep_server)
+    example_code_system_source = prep_server['CMG']['ConceptMap'][0]
     result = host.post('ConceptMap', example_code_system_source, validate_only=False)
     assert result['status_code'] == 201
     cm_id = result['response']['id']
 
     # While we have a concept map in place, let's do a few checks
-    translate_result = host.get(f"ConceptMap/$translate?system=http://hl7.org/fhir/administrative-gender&code=male", no_count=True).entries[0]
+    translate_result = host.get(f"ConceptMap/$translate?system=http://hl7.org/fhir/administrative-gender&code=male", rec_count=-1).entries[0]
     match_result = ConceptMapParameter(translate_result)
     assert match_result.match_count > 0, "Make sure we have at least one result"
     assert match_result.result, "Make sure the first match is a postive"
