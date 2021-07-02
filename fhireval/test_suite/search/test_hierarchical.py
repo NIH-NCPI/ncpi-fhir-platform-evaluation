26a37fe1ed3fee1950c2765f67ddf2ab8b0029a1
Eric Torstenson <eric.s.torstenson@vumc.org>
2021-07-02 14:15:05 -0500

    Removed test_code_system_without_system because it is a meaningless test

--- fhireval/test_suite/search/test_hierarchical.py
+++ fhireval/test_suite/search/test_hierarchical.py
@@ -30,27 +30,3 @@ def test_code_system(host):
     assert parent_count > 0, "Make sure we have non-zero parent count"
     response = host.get("Observation?value-concept:below=http://terminology.hl7.org/CodeSystem/v3-RoleCode|PRN&_total=accurate").response
     assert response['total'] == parent_count, f"Do our counts agree? ({parent_count} == {response['total']})"
-
-
-def test_code_system_without_system(host):
-    assert 0 == 1, "on SMILES, this creates a weird infinite loop that takes forever to complete (and fail), so I'm turning it off until that's resolved"
-"""
-def test_code_system_without_system(host):
-    # We'll seach for all parents, despite (hopefully) having no one marked as PRN
-    response = host.get(f"Observation?value-concept=PRN&_total=accurate").response
-    #assert response['total'] == 0, "Make sure we don't have anyone marked with a family relationship as PRN (parent)"
-
-    parent_count = 0
-    response = host.get(f"Observation?code=FAMMEMB&_total=accurate").response
-    for entry in response['entry']:
-        #pdb.set_trace()
-        code = CodeableConcept(block=entry['resource']['valueCodeableConcept'])
-
-        # Right now, all of our data is either MTH or FTH
-        if code == 'FTH' or code == 'MTH':
-            parent_count += 1
-
-    assert parent_count > 0, "Make sure we have non-zero parent count"
-    response = host.get("Observation?value-concept:below=PRN&_total=accurate").response
-    assert response['total'] == parent_count, f"Do our counts agree? ({parent_count} == {response['total']})"
-"""
