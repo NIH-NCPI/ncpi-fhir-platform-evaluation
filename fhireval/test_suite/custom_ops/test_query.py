90435023c392cfa85be537a9386c9f8f053ee29b
Eric Torstenson <eric.s.torstenson@vumc.org>
2021-07-02 10:34:56 -0500

    moved this test into custom ops since it depends on custom operations to work

---
+++ fhireval/test_suite/custom_ops/test_query.py
@@ -0,0 +1,12 @@
+import pytest
+import pdb
+
+from fhir_walk.model.patient import Patient
+
+test_id = f"{'2.6.3':<10} - _query"
+
+test_weight = 8
+
+def test_query(host):
+    assert 0 == 1, "TODO - Need a perform a _query against a custom operation"
+
